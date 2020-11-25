53602af0cddf4d2442db6b9a6f0e1f2a01315bcc
Joao Zsigmond <jzsiggy@gmail.com>
2020-11-24 21:29:46 -0300

    Adding whitelist as env var

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended : false }));
 app.use(bodyParser.json());
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.PORT,
     credentials: true
 }))
 
